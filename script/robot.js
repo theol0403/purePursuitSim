b5740c9f18e4e46c7c55ba884f116b21c30d1ef8
theol0403 <theol0403@gmail.com>
2019-06-16 17:42:12 -0600

    forgot to stage robot

---
+++ script/robot.js
@@ -0,0 +1,95 @@
+
+const SPEED_LIM = 0.4;
+const TURN_MAG = 3;
+
+const PI = Math.PI;
+const TAU = Math.PI * 2;
+
+const sgn = v => (v > 0 ? 1 : v < 0 ? -1 : 0);
+const rollAngle360 = angle => angle - TAU * Math.floor(angle / TAU);
+const rollAngle180 = angle => angle - TAU * Math.floor((angle + PI) / TAU);
+
+class Bot {
+  constructor(x, y, a) {
+    this.pos = { x: x, y: y, a: a };
+    this.vel = { x: 0, y: 0 };
+    this.acc = { x: 0, y: 0 };
+    this.spd = { l: 0, r: 0 };
+  }
+
+  tank(left, right) { this.spd = { l: left, r: right }; }
+
+  getPos() {
+    return this.pos;
+  }
+
+  getHeading() {
+    return -this.pos.a;
+  }
+
+  update() {
+    let dA = (this.spd.r - this.spd.l) * SPEED_LIM / PI;
+    this.pos.a -= dA;
+    this.pos.a = rollAngle180(this.pos.a);
+
+    this.vel.x = Math.cos(this.pos.a) * (this.spd.r + this.spd.l) * SPEED_LIM;
+    this.vel.y = Math.sin(this.pos.a) * (this.spd.r + this.spd.l) * SPEED_LIM;
+
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+
+    this.draw();
+  }
+
+  draw() {
+    c.fillStyle = "#000";
+    c.beginPath();
+    c.arc(this.pos.x, this.pos.y, 4, 0, Math.PI * 2);
+    c.closePath();
+    c.fill();
+
+    c.strokeStyle = "#000";
+    c.beginPath();
+    c.moveTo(this.pos.x, this.pos.y);
+    c.lineTo(
+      this.pos.x + Math.cos(this.pos.a) * 10,
+      this.pos.y + Math.sin(this.pos.a) * 10
+      );
+    c.closePath();
+    c.stroke();
+
+    c.strokeStyle = "#0F0";
+    c.beginPath();
+    c.moveTo(
+      this.pos.x + Math.cos(this.pos.a + Math.PI / 2) * 10,
+      this.pos.y + Math.sin(this.pos.a + Math.PI / 2) * 10
+      );
+    c.lineTo(
+      this.pos.x +
+      Math.cos(this.pos.a + Math.PI / 2) * 10 +
+      this.spd.r * 20 * Math.cos(this.pos.a),
+      this.pos.y +
+      Math.sin(this.pos.a + Math.PI / 2) * 10 +
+      this.spd.r * 20 * Math.sin(this.pos.a)
+      );
+    c.closePath();
+    c.stroke();
+
+    c.strokeStyle = "#0F0";
+    c.beginPath();
+    c.moveTo(
+      this.pos.x + Math.cos(this.pos.a - Math.PI / 2) * 10,
+      this.pos.y + Math.sin(this.pos.a - Math.PI / 2) * 10
+      );
+    c.lineTo(
+      this.pos.x +
+      Math.cos(this.pos.a - Math.PI / 2) * 10 +
+      this.spd.l * 20 * Math.cos(this.pos.a),
+      this.pos.y +
+      Math.sin(this.pos.a - Math.PI / 2) * 10 +
+      this.spd.l * 20 * Math.sin(this.pos.a)
+      );
+    c.closePath();
+    c.stroke();
+  }
+}
