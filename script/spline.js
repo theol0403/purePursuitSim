50e577daeb6995c84af4c76947c2ce9a391b9f4e
Rahul Menon <menonrahul02@gmail.com>
2020-01-04 22:25:18 -0600

    Redo spline calculations using WPILib algorithms

--- script/spline.js
+++ script/spline.js
@@ -1,88 +1,76 @@
 
-class QuinticPolynomial {
-  
-  constructor(xstart, vstart, xend, vend) {
-    let T = 1;
+class QuinticSpline {
 
-    let A = math.matrix([[Math.pow(T, 3), Math.pow(T, 4), Math.pow(T, 5)],
-                         [3 * T*T, 4 * Math.pow(T, 3), 5 * Math.pow(T, 4)],
-                         [6 * T, 12 * T*T, 20 * Math.pow(T, 3)]]);
-    let b = math.matrix([[xend - xstart - vstart * T],
-                         [vend - vstart],
-                         [0]]);
+  constructor(waypoints) {
+    this.waypoints = waypoints;
 
-    let x = math.lusolve(A, b);
-
-    this.a3 = math.subset(x, math.index(0, 0));
-    this.a4 = math.subset(x, math.index(1, 0));
-    this.a5 = math.subset(x, math.index(2, 0));
-
-    this.coeffs = [xstart, vstart, 0, this.a3, this.a4, this.a5];
+    this._generateControlVectors();
+    this._generateSplines();
   }
 
-  calcPoint(t) {
-    let xt = 0;
-    for(let power = 0; power < 6; power++) {
-      xt += this.coeffs[power] * Math.pow(t, power);
-    }
-
-    return xt;
-  }
-}
-
-class QuinticSegmentPlanner {
-
-  constructor(s, g, dt) {
-    let vxs = s.vel * Math.cos(s.theta);
-    let vys = s.vel * Math.sin(s.theta);
-    let vxg = g.vel * Math.cos(g.theta);
-    let vyg = g.vel * Math.sin(g.theta);
+  _generateControlVectors() {
+    let vectors = [];
     
-    let xqp = new QuinticPolynomial(s.x, vxs, g.x, vxg);
-    let yqp = new QuinticPolynomial(s.y, vys, g.y, vyg);
+    for(let i = 0; i < length(this.waypoints) - 1; i++) {
+      let p0 = this.waypoints[i];
+      let p1 = this.waypoints[i+1];
 
-    this.rx = [];
-    this.ry = [];
+      let scalar = 1.2 * Vector.dist(p0.vector(), p1.vector());
 
-    for(let t = 0; t <= 1; t += dt) {
-      this.rx.push(xqp.calcPoint(t));
-      this.ry.push(yqp.calcPoint(t));
-    }
-  }
-
-  getPath() {
-    let path = []
-
-    for(let i = 0; i < this.rx.length; i++) {
-      let p = new PathPoint(this.rx[i], this.ry[i]);
-      path.push(p);
+      vectors.push(this._createControlVector(scalar, p0));
+      vectors.push(this._createControlVector(scalar, p1));
     }
 
-    return path;
+    this.controlVectors = vectors;
   }
-}
 
-class QuinticPathPlanner {
+  _generateSplines() {
+    let splines = [];
 
-  constructor(points, dt=0.01) {
-    if(points.length == 2) {
-      let [p1, p2] = points;
-      let segment = new QuinticSegmentPlanner(p1, p2, dt);
+    for(let i = 0; i < length(this.controlVectors) - 1; i++) {
+      let xInitial = this.controlVectors[i].x;
+      let yInitial = this.controlVectors[i].y;
+      let xFinal = this.controlVectors[i+1].x;
+      let yFinal = this.controlVectors[i+1].y;
 
-      this.path = segment.getPath();
-    } else {
-      this.path = [];
-      for(let i = 0; i < points.length - 1; i++) {
-        let p1 = points[i];
-        let p2 = points[i+1];
-        let segment = new QuinticSegmentPlanner(p1, p2, dt);
-
-        this.path = this.path.concat(segment.getPath());
-      }
+      splines.push(_this.generateSpline(xInitial, xFinal, yInitial, yFinal));
     }
+
+    this.splines = splines;
   }
 
-  getPath() {
-    return this.path;
+  _createControlVector(scalar, point) {
+    return {x: [point.x, scalar * Math.cos(point.theta), 0],
+            y: [point.y, scalar * Math.sin(point.theta), 0]};
+  }
+
+  _generateSpline(xInitial, xFinal, yInitial, yFinal) {
+    let coeffs = math.zeros([6, 6]);
+
+    let hermite = [[ -6, -3, -0.5,   6, -3, 0.5],
+                   [ 15,  8,  1.5, -15,  7,   1],
+                   [-10, -6, -1.5,  10, -4, 0.5],
+                   [  0,  0,  0.5,   0,  0,   0],
+                   [  0,  1,    0,   0,  0,   0],
+                   [  1,  0,    0,   0,  0,  0]];
+
+    let x = [[xInitial[0]], [xInitial[1]], [xInitial[2]], 
+             [xFinal[0]], [xFinal[1]], [xFinal[2]]];
+
+    let y = [[yInitial[0]], [yInitial[1]], [yInitial[2]], 
+             [yFinal[0]], [yFinal[1]], [yFinal[2]]];
+
+    coeffs.subset(math.index(0), math.transpose(hermite * x));
+    coeffs.subset(math.index(1), math.transpose(hermite * y));
+
+    for(let i = 0; i < 6; i++) {
+      coeffs.subset(math.index([2, 3], i), math.subset(coeffs, math.index([0, 1], i) * (5-i)));
+    }
+
+    for(let i = 0; i < 5; i ++) {
+      coeffs.subset(math.index([4, 5], i), math.subset(coeffs, math.index([2, 3], i) * (4-i)));
+    }
+
+    this.coeffs = coeffs;
   }
 }
